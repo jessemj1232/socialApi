ee59e12e1c83d5b3297539bf588778f82fe65707
Jesse Jones <jessejones1009910@gmail.com>
2018-06-22 17:11:16 -0700

    project setup

---
+++ socialApi/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace socialApi.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string RoleType { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public char Gender { get; set; }
+        public string Occupation { get; set; }
+
+    }
+}
